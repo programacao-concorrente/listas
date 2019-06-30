7a4f0a0b3231cc62a9d5159c752099625e4a4262
Armando Soares Sousa <armando.sousa@gmail.com>
2019-06-30 15:01:09 -0300

    Update Questao 5-6 Filosofos com monitores.c

--- lista4/Questao 5-6 Filosofos com monitores.c
+++ lista4/Questao 5-6 Filosofos com monitores.c
@@ -4,7 +4,7 @@ monitor DP
 { 	
 	status = enum('hungry', 'eating', 'thinking');	
 	/*
-	status(1) = 'hungry', status(2) = 'eating', status(3) = 'thinking'	
+	status(0) = 'hungry', status(1) = 'eating', status(2) = 'thinking'	
 	*/
 	char state[5]; 
 	condition self[5]; 
@@ -13,7 +13,7 @@ monitor DP
 	request_forks(int i) 
 	{ 
 		// indicate that I’m hungry 
-		state[i] = status(1); //hungry; 
+		state[i] = status(0); //hungry; 
 
 		// set state to eating in test() 
 		// only if my left and right neighbors 
@@ -21,7 +21,7 @@ monitor DP
 		test(i); 
 
 		// if unable to eat, wait to be signaled 
-		if (state[i] != status(2)) // state[i] != eating 
+		if (state[i] != status(1)) // state[i] != eating 
 			wait(self[i]); 
 	} 
 
@@ -30,7 +30,7 @@ monitor DP
 	{ 
 
 		// indicate that I’m thinking 
-		state[i] = status(3); //thinking; 
+		state[i] = status(2); //thinking; 
 
 		// if right neighbor R=(i+1)%5 is hungry and 
 		// both of R’s neighbors are not eating, 
@@ -43,9 +43,9 @@ monitor DP
 	test(int i) 
 	{ 
 		//Checa se o vizinho da direita nao esta comendo e se o vizionho da direita nao esta comendo e o requisitante esta com fome
-		if ((state[(i + 1) % 5] != status(2)) && (state[(i + 4) % 5] != status(2)) && (state[i] == status(1))) { 
+		if ((state[(i + 1) % 5] != status(1)) && (state[(i + 4) % 5] != status(1)) && (state[i] == status(0))) { 
 			// indicate that I’m eating 
-			state[i] = status(2); 
+			state[i] = status(1); 
 			// signal() has no effect during Pickup(), 
 			// but is important to wake up waiting 
 			// hungry philosophers during Putdown() 
@@ -63,7 +63,7 @@ monitor DP
 		// Verify that this monitor-based solution is 
 		// deadlock free and mutually exclusive in that 
 		// no 2 neighbors can eat simultaneously 
-			state[i] = status(3);//thinking 
+			state[i] = status(2);//thinking 
 		}
 	} 
 } // end of monitor 
